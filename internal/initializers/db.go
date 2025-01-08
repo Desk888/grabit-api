ec388787536d3d38065a57a31196f16cbe798de4
Desk888 <work.lorenzofilippinI@gmail.com>
2025-01-08 15:21:26 +0000

    Refactor db.go to use environment variables for database connection configuration, enhancing flexibility and security in database initialization.

--- internal/initializers/db.go
+++ internal/initializers/db.go
@@ -2,7 +2,7 @@ package initializers
 
 import (
 	"log"
-
+	"os"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
@@ -11,7 +11,7 @@ var DB *gorm.DB
 
 func InitDB() {
 	var err error
-	connStr := "host=db port=5432 user=postgres password=postgres dbname=postgres sslmode=disable"
+	connStr := "host=db port=" + os.Getenv("DB_PORT") + " user=" + os.Getenv("DB_USER") + " password=" + os.Getenv("DB_PASSWORD") + " dbname=" + os.Getenv("DB_NAME") + " sslmode=disable"
 	DB, err = gorm.Open(postgres.Open(connStr), &gorm.Config{})
 	if err != nil {
 		log.Fatal("Failed to connect to the database:", err)
