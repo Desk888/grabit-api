3bd365d47b5be86eeab054c807ee13854abf0b31
Desk888 <work.lorenzofilippinI@gmail.com>
2025-01-10 01:25:39 +0000

    REFactor: Replace hardcoded database connection string with environment variables for improved security and flexibility

--- internal/initializers/Db.go
+++ internal/initializers/Db.go
@@ -4,13 +4,19 @@ import (
 	"log"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+	"os"
 )
 
 var DB *gorm.DB
 
 func InitDB() {
 	var err error
-	connStr := "host=db port=5432 user=postgres password=postgres dbname=postgres sslmode=disable"
+	connStr := "host=" + os.Getenv("DB_HOST") + 
+		" port=" + os.Getenv("DB_PORT") + 
+		" user=" + os.Getenv("DB_USER") + 
+		" password=" + os.Getenv("DB_PASSWORD") + 
+		" dbname=" + os.Getenv("DB_NAME") + 
+		" sslmode=" + os.Getenv("DB_SSLMODE")
 	DB, err = gorm.Open(postgres.Open(connStr), &gorm.Config{})
 	if err != nil {
 		log.Fatal("Failed to connect to the database:", err)
